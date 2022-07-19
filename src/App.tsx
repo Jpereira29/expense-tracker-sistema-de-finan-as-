8eca0cf234cfdd306317b5a4e4060c48c1c93b4a
jorge <jorge@pop-os.localdomain>
2022-07-19 12:39:16 +0000

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as C from "./App.styles";
 
-function App() {
+import { items } from './data/items'
+import { Item } from './types/Item'
+import { useEffect, useState } from "react";
+
+import { filterListByMonth, getCurrentMonth } from './helpers/dateFilter'
+import { TableArea } from "./components/TableArea";
+import { InfoArea } from "./components/InfoArea";
+import { categories } from "./data/categories";
+import { InputArea } from "./components/InputArea";
+
+export default function App() {
+
+  const [list, setList] = useState(items)
+  const [filteredList, setFilteredList] = useState<Item[]>([])
+  const [currentMonth, setCurrentMonth] = useState(getCurrentMonth())
+  const [income, setIncome] = useState(0)
+  const [expense, setExpense] = useState(0)
+
+  useEffect(() => {
+    setFilteredList(filterListByMonth(list, currentMonth))
+  }, [list, currentMonth])
+
+  useEffect(() => {
+    let incomeCount = 0
+    let expenseCount = 0
+
+    for (let i in filteredList) {
+      if (categories[filteredList[i].category].expense) {
+        expenseCount += filteredList[i].value
+      } else {
+        incomeCount += filteredList[i].value
+      }
+    }
+
+    setIncome(incomeCount)
+    setExpense(expenseCount)
+
+  }, [filteredList])
+
+  function handleMonhChange(newMonth: string) {
+    setCurrentMonth(newMonth)
+  }
+
+  function handleAddItem(item: Item) {
+    let newList = [...list]
+    newList.push(item)
+    setList(newList)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <C.Container>
+      <C.Header>
+        <C.HeaderText>Sistema Financeiro</C.HeaderText>
+      </C.Header>
 
-export default App;
+      <C.Body>
+        <InfoArea
+          currentMonth={currentMonth}
+          onMonthChange={handleMonhChange}
+          income={income}
+          expense={expense}
+        />
+
+        <InputArea onAdd={handleAddItem} />
+
+        <TableArea list={filteredList} />
+      </C.Body>
+    </C.Container>
+  )
+}
